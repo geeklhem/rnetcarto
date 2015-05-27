b057c9658217644083e5e7110130467dac734b73
Guilhem Doulcier <guilhem.doulcier@ens.fr>
2015-05-27 15:19:23 +1200

    Removed unused variables and set unset variables.
    As seen when compiling with the -Wall option.

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@
 #include <search.h>
 
 #include "rgraph/src/tools.h"
+#include "rgraph/src/louvain.h"
 #include "rgraph/src/graph.h"
 #include "rgraph/src/datastruct.h"
 #include "rgraph/src/modules.h"
@@ -36,26 +37,26 @@ Compute the modules, modularity and modularity roles of a network.
 @param nodes_in,nodes_out Atomic integer vectors giving the id of the nodes.
 @param weight Atomic numeric vector giving the weight of the edges.
 @param r_seed Seed for the random number generator: Must be a positive
-integer. 
+integer.
 @param r_iterfac At each temperature of the simulated annealing
 (SA), the program performs fN^2 individual-node updates (involving the
 movement of a single node from one module to another) and fN
 collective updates (involving the merging of two modules and the split
 of a module). The number "f" is the iteration factor.
 @param r_symmetric If !=0 all edges a->b are copied b->a.
-@param r_coolingfac Temperature cooling factor. 
+@param r_coolingfac Temperature cooling factor.
 @param r_auto_link If !=0 allows self looping edges a->a
 @param r_add_weight If !=0 weights are summed if the edge already exist.
 @return A list containing the 1) module, 2) within module z-score, 3)
-        participation coefficient for each node and 4) modularity.
+		participation coefficient for each node and 4) modularity.
  */
 SEXP CNetcarto(SEXP nodes_in, SEXP nodes_out, SEXP weight,  SEXP r_coolingfac, SEXP r_seed,
 			  SEXP r_iterfac, SEXP r_symmetric, SEXP r_auto_link, SEXP r_add_weight){
- 
+
   // Arguments
   int E = LENGTH(nodes_in); // Number of edges
   double iterfac = REAL(r_iterfac)[0];
-  double coolingfac = REAL(r_coolingfac)[0];  
+  double coolingfac = REAL(r_coolingfac)[0];
   long seed = INTEGER(r_seed)[0];
   int symmetric = INTEGER(r_symmetric)[0];
   int auto_link = INTEGER(r_auto_link)[0];
@@ -71,19 +72,19 @@ SEXP CNetcarto(SEXP nodes_in, SEXP nodes_out, SEXP weight,  SEXP r_coolingfac, S
   int N, mod_nb = 0;
   SEXP ans, module, z, P, modularity;
 
-  
+
   //// RANDOM NUMBER GENERATOR INITIALIZATION
   rand_gen = gsl_rng_alloc(gsl_rng_mt19937);
   gsl_rng_set(rand_gen, seed);
-  
-  //// READ INPUT AND BUILD GRAPH 
- 
+
+  //// READ INPUT AND BUILD GRAPH
+
   network = ABuildNetwork(E, INTEGER(nodes_in), INTEGER(nodes_out),
 						  REAL(weight),
 						  symmetric, auto_link, add_weight);
   N = CountNodes(network);
-  
-  //// BUILD OUTPUT STRUCTURE 
+
+  //// BUILD OUTPUT STRUCTURE
   PROTECT(ans = allocVector(VECSXP,4));
   PROTECT(module = allocVector(INTSXP,N));
   PROTECT(z = allocVector(REALSXP,N));
@@ -95,46 +96,46 @@ SEXP CNetcarto(SEXP nodes_in, SEXP nodes_out, SEXP weight,  SEXP r_coolingfac, S
   SET_VECTOR_ELT(ans,3,modularity);
 
   //// COMPUTE RESULTS
- 
+
   part = SACommunityIdent(network,
 						  2.0 / (double)N, Tf, coolingfac,
 						  iterfac, 0, 'o', 1, 'n', rand_gen);
- 
-  
+
+
   //// RETURN RESULTS
-  // Get partition modularity. 
+  // Get partition modularity.
   REAL(modularity)[0] = Modularity(part);
 
-  // Get the node metrics. 
+  // Get the node metrics.
   g = part;
   while ((g = g->next) != NULL) { // Modules
 	mod_nb++;
-    p = g->nodeList;
-    while ((p = p->next) != NULL) { // Nodes in this module.
+	p = g->nodeList;
+	while ((p = p->next) != NULL) { // Nodes in this module.
 	  INTEGER(module)[p->node] = mod_nb;
-      REAL(P)[p->node] = ParticipationCoefficient(p->ref);
-      REAL(z)[p->node] = WithinModuleRelativeDegree(p->ref, g);
+	  REAL(P)[p->node] = ParticipationCoefficient(p->ref);
+	  REAL(z)[p->node] = WithinModuleRelativeDegree(p->ref, g);
 	}
   }
-  
+
   /// Free Memory
   gsl_rng_free(rand_gen);
   RemovePartition(part);
   RemoveGraph(network);
-  UNPROTECT(5);  
+  UNPROTECT(5);
   return ans;
 }
 
 /**
 Array Build Network
- 
+
 Build a network from a list of edges. Each edge i is defined by a
 entry node, nd_in[i], a exit node nd_out[i] and a weight, weight[i].
 
 The nodes id must be integer in [1, |Nodes|-1].
 
 @param E number of edges
-@param nd_in,nd_out list of size E, id of the nodes 
+@param nd_in,nd_out list of size E, id of the nodes
 @param weight weight of the edges
 @param symmetric if !=0 all edges a->b are copied b->a.
 @param auto_link_sw allows auto link if !=0
@@ -150,10 +151,9 @@ struct node_gra *ABuildNetwork(int E,
   struct node_gra *n1=NULL, *n2=NULL;
   struct node_gra *root=NULL, *last_add=NULL;
   void *node_dict=NULL;
-  struct node_tree *n_tree=NULL, *ntree1=NULL, *ntree2=NULL;
   int N = 0;
   char buffer[255];
-  
+
   // Create the header of the graph
   root = last_add = CreateHeaderGraph();
 
@@ -167,25 +167,21 @@ struct node_gra *ABuildNetwork(int E,
 	}
   N ++; //Because node index start at 0.
 
-  // Add the nodes 
+  // Add the nodes
   for (int i=0; i<N; ++i)
 	{
 	  sprintf (buffer,"nd_%d",i);
 	  last_add = CreateNodeGraph(last_add, buffer);
-	  //printf ("Create node %d, num:  %d, label %s\n", i, last_add->num,last_add->label);
 	}
 
-  // Add the edges 
+  // Add the edges
   for (int i = 0; i < E; ++i)
 	{
-	  //printf ("### Add edge %d/%d between %d and %d \n",i,E,nd_in[i],nd_out[i]);
 	  n1 = GetNode(nd_in[i],root);
 	  n2 = GetNode(nd_out[i],root);
-	  //printf ("Got nodes %d (%s) and %d (%s) \n",n1->num,n1->label,n2->num,n2->label);
 	  AddAdjacency(n1, n2, auto_link_sw, add_weight_sw, weight[i], 0);
 	  if (symmetric != 0 && n1 != n2)
 		  AddAdjacency(n2, n1, auto_link_sw, add_weight_sw, weight[i], 0);
-	  //printf ("OK.\n");
 	}
   return root;
 }
@@ -200,7 +196,7 @@ and a weight, weight[i].
 The nodes id must be integer in [0, |Nodes|-1].
 
 @param E number of edges
-@param nd_part1,nd_part2 list of size E, id of the nodes 
+@param nd_part1,nd_part2 list of size E, id of the nodes
 @param weight weight of the edges
 @param add_weight if !=0, weights are summed if the edge already exist.
 */
@@ -215,8 +211,8 @@ struct binet *ABuildNetworkBipart(int E,
   struct node_gra *n1=NULL, *n2=NULL;
   struct binet *net=NULL;
   int N1 = 0, N2 = 0;
-  char buffer[255]; 
-    
+  char buffer[255];
+
   // Initialize the networks
   net = CreateBipart();
   last1 = root1 = CreateHeaderGraph();
@@ -227,7 +223,6 @@ struct binet *ABuildNetworkBipart(int E,
   // Get the number of nodes
   for (int i = 0; i < E; ++i)
 	{
-	  //printf ("Edge %d, (%d,%d)\n",i,nd_part1[i],nd_part2[i]);
 	  if(nd_part1[i]>N1)
 		N1 = nd_part1[i];
 	  if(nd_part2[i]>N2)
@@ -235,27 +230,24 @@ struct binet *ABuildNetworkBipart(int E,
 	}
   N1 ++; //Because node index start at 0.
   N2 ++; //Because node index start at 0.
-  //printf ("N1 %d N2 %d\n",N1,N2 );
 
-  // Add the nodes 
+  // Add the nodes
   for (int i=0; i<N1; ++i)
 	{
 	  sprintf (buffer,"nd_1_%d",i);
 	  last1 = CreateNodeGraph(last1, buffer);
-	  //printf ("Create node %d, num:  %d, label %s\n", i, last1->num,last1->label);
 	}
   for (int i=0; i<N2; ++i)
 	{
 	  sprintf (buffer,"nd_2_%d",i);
 	  last2 = CreateNodeGraph(last2, buffer);
-	  //printf ("Create node %d, num:  %d, label %s\n", i, last2->num,last2->label);
 	}
 
   // Add the edges
   for (int i = 0; i < E; ++i)
 	{
 	  n1 = GetNode(nd_part1[i],root1);
-	  n2 = GetNode(nd_part2[i],root2); 
+	  n2 = GetNode(nd_part2[i],root2);
 	  AddAdjacency(n1, n2, 0, add_weight_sw, weight[i], 0);
 	  AddAdjacency(n2, n1, 0, add_weight_sw, weight[i], 0);
 	}
@@ -272,17 +264,17 @@ Compute the modules, modularity and modularity roles of a bipartite network.
 @param weight Atomic numeric vector giving the weight of the edges.
 @param weighted_modularity if True the weighted formula for modularity is used.
 @param r_seed Seed for the random number generator: Must be a positive
-integer. 
+integer.
 @param r_iterfac At each temperature of the simulated annealing
 (SA), the program performs fN^2 individual-node updates (involving the
 movement of a single node from one module to another) and fN
 collective updates (involving the merging of two modules and the split
 of a module). The number "f" is the iteration factor.
-@param r_coolingfac Temperature cooling factor. 
+@param r_coolingfac Temperature cooling factor.
 @param r_degree_based If true use the degree based formula.
 @param r_weighted If true use the weighted version of the formula.
 @return A list containing the 1) module, 2) within module z-score, 3)
-        participation coefficient for each node and 4) modularity.
+		participation coefficient for each node and 4) modularity.
  */
 SEXP CBipartmod(SEXP nodes1, SEXP nodes2, SEXP weight, SEXP r_seed,
 				SEXP r_iterfac, SEXP r_coolingfac,
@@ -296,9 +288,9 @@ SEXP CBipartmod(SEXP nodes1, SEXP nodes2, SEXP weight, SEXP r_seed,
   struct group *g = NULL;
   struct node_gra *projected = NULL;
   gsl_rng *rand_gen;
-  
+
   double Ti = 0.0, Tf = 0.0 ;
-  int  N1,N2, mod_nb = 0;
+  int  N1, mod_nb = 0;
 
   // Argument
   int E = LENGTH(nodes1); // Number of edges
@@ -308,21 +300,18 @@ SEXP CBipartmod(SEXP nodes1, SEXP nodes2, SEXP weight, SEXP r_seed,
   int add_weight = INTEGER(r_add_weight)[0];
   int weighted = INTEGER(r_weighted)[0];
   int degree_based = INTEGER(r_degree_based)[0];
-  
+
   //// RANDOM NUMBER GENERATOR INITIALIZATION
   rand_gen = gsl_rng_alloc(gsl_rng_mt19937);
   gsl_rng_set(rand_gen, seed);
-  
-  //// READ INPUT AND BUILD GRAPH 
-  //printf("----- Building the network from %d edges.\n",E);
+
+  //// READ INPUT AND BUILD GRAPH
   network = ABuildNetworkBipart(E,INTEGER(nodes1),INTEGER(nodes2),
 								REAL(weight),
 								add_weight);
   N1 = CountNodes(network->net1);
-  N2 = CountNodes(network->net2);
-  //printf("----- The network has %d,%d nodes\n", N1,N2);
-  
-  //// BUILD OUTPUT STRUCTURE 
+
+  //// BUILD OUTPUT STRUCTURE
   PROTECT(ans = allocVector(VECSXP,4));
   PROTECT(module = allocVector(INTSXP,N1));
   PROTECT(z = allocVector(REALSXP,N1));
@@ -334,7 +323,6 @@ SEXP CBipartmod(SEXP nodes1, SEXP nodes2, SEXP weight, SEXP r_seed,
   SET_VECTOR_ELT(ans,3,modularity);
 
   //// COMPUTE RESULTS
-  //printf("----- Simulated Annealing \n");
   Ti = 1. / (double)N1;
 
   if (weighted == 1)
@@ -344,30 +332,27 @@ SEXP CBipartmod(SEXP nodes1, SEXP nodes2, SEXP weight, SEXP r_seed,
 										  rand_gen);
   else
 	modules = SACommunityIdentBipart(network,
-								  Ti, Tf, Tsched, iterfac,
-								  0, 'o', 1, 'n',
-								  rand_gen);
-  //printf("----- Done.\n");
-  
+									 Ti, Tf, Tsched, iterfac,
+									 0, 'o', 1, 'n',
+									 rand_gen);
+
   //// RETURN RESULTS
-  // Get partition modularity. 
+  // Get partition modularity.
   REAL(modularity)[0] = ModularityBipart(network,modules);
-  
+
   projected = ProjectBipart(network);
   MapPartToNetFast(modules, projected);
 
-  // Get the node metrics. 
+  // Get the node metrics.
   g = modules;
   while ((g = g->next) != NULL) { // Modules
-	//printf ("--Module %d\n",mod_nb);
 	mod_nb++;
-    n = g->nodeList;
-    while ((n = n->next) != NULL) { // Nodes in this module.
-	  //printf ("-----Node %d\n",n->node);
+	n = g->nodeList;
+	while ((n = n->next) != NULL) { // Nodes in this module.
 	  INTEGER(module)[n->node] = mod_nb;
 	  if (degree_based == 1){
-      REAL(P)[n->node] = ParticipationCoefficient(n->ref);
-      REAL(z)[n->node] = WithinModuleRelativeDegree(n->ref, g);
+	  REAL(P)[n->node] = ParticipationCoefficient(n->ref);
+	  REAL(z)[n->node] = WithinModuleRelativeDegree(n->ref, g);
 	  }
 	  else{
 		REAL(P)[n->node] = WeightedParticipationCoefficient(n->ref,modules);
@@ -375,12 +360,12 @@ SEXP CBipartmod(SEXP nodes1, SEXP nodes2, SEXP weight, SEXP r_seed,
 	  }
 	}
   }
-  
+
   /// Free Memory
   gsl_rng_free(rand_gen);
   RemovePartition(modules);
   RemoveBipart(network);
   RemoveGraph(projected);
-  UNPROTECT(5);  
+  UNPROTECT(5);
   return ans;
 }
